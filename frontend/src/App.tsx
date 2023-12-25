a51378a4ec5d90382948bd34a4a73d295700f76a
Linda <lingo7831@gmail.com>
2023-12-25 18:11:19 -0500

    starter files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Wallet from "../src/modules/wallet/components/Wallet";
+import Nav from "../src/modules/wallet/components/Nav";
+import {
+  Container,
+  WalletContainer,
+  GlobalStyle,
+} from "../src/modules/common/styles";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const GlobalStyleProxy: any = GlobalStyle;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GlobalStyleProxy />
+      <Container>
+        <Nav />
+        <WalletContainer>
+          <Wallet />
+        </WalletContainer>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
