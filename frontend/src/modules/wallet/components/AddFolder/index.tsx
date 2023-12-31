27cceb1be56f4b10f812d6a4f5842e2821e9c25c
Linda <lingo7831@gmail.com>
2023-12-30 23:48:24 -0500

    add form for add folder

--- frontend/src/modules/wallet/components/AddFolder/index.tsx
+++ frontend/src/modules/wallet/components/AddFolder/index.tsx
@@ -1,14 +1,10 @@
-import { FolderStyle, Title} from "./styles";
+import { FolderStyle, Input, Title, Form} from "./styles";
 import { useState } from "react";
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, FormikHelpers } from 'formik';
 import { useSpring } from "@react-spring/web";
 
 const AddFolder = () => {
   const [openAddFolder, setOpenAddFolder] = useState(false);
-
-  const handleToggle = () => {
-    setOpenAddFolder(!openAddFolder);
-  };
 
   const addFolderProps = useSpring({
     maxHeight: openAddFolder ? 1000 : 70,
@@ -17,36 +13,47 @@ const AddFolder = () => {
       : {duration: 400, tension: 10, friction: 10},
   });
 
+  const handleToggle = () => {
+    setOpenAddFolder(!openAddFolder);
+  };
+
+  interface FormValues {
+    title: string;
+  }
+
+  const handleSubmit = (values: FormValues, {setSubmitting, resetForm}: FormikHelpers<FormValues>) => {
+    setTimeout(() => {
+      setSubmitting(false);
+      handleToggle();
+      localStorage.setItem(values.title, JSON.stringify([]));
+      resetForm();
+    }, 400)
+  }
+
   return (
     <>
       <FolderStyle style={addFolderProps}>
         <Title onClick={handleToggle}>+ Add folder</Title>
         <Formik
-          initialValues={{ email: '', password: '' }}
+          initialValues={{ title: '' }}
           validate={values => {
-            const errors = {};
-            if (!values.email) {
-              errors.email = 'Required';
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-            ) {
-              errors.email = 'Invalid email address';
+            const errors: Partial<FormValues> = {};
+            if (!values.title) {
+              errors.title = 'Title is required';
             }
             return errors;
           }}
-          onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-              alert(JSON.stringify(values, null, 2));
-              setSubmitting(false);
-            }, 400);
-          }}
+          onSubmit={handleSubmit}
         >
-          {({ isSubmitting }) => (
-            <Form>
-              <Field type="email" name="email" />
-              <ErrorMessage name="email" component="div" />
-              <Field type="password" name="password" />
-              <ErrorMessage name="password" component="div" />
+          {({ values, handleChange, isSubmitting }) => (
+            <Form onSubmit={handleSubmit}>
+              <Input
+                type="text"
+                name="title"
+                onChange={handleChange}
+                value={values.title}
+                placeholder="folder title"
+              />
               <button type="submit" disabled={isSubmitting}>
                 Submit
               </button>
